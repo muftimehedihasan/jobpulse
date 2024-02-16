6d651fe6823b5b05f905f17fcc39f2fb153e98ee
unknown <mehedihasan.projects@gmail.com>
2024-02-16 02:00:08 -0800

    first commit

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+
+    public function index()
+    {
+        return view('listings.index');
+    }
+}
